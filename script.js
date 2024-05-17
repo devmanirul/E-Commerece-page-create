bd523b005be5e00bef6036d336d528acab4e0af2
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-05-17 09:50:20 +0600

    update

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  $(".testing").slick({
+    //   setting-name: setting-value
+    arrows: true,
+    slideToShow: 4, 
+    slidesToScroll: 1,
+    prevArrow: ".previous",
+    nextArrow: ".next",
+
+
+  });
+});
+
+//   $(document).ready(function(){
+//     $('.catagoryCard').slick({
+//     //   setting-name: setting-value
+
+//     slidesToShow: 4,
+//     slidesToScroll: 1,
+//     });
+//   });
