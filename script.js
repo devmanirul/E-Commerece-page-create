21276c7da99366b0a1689a95179e96446ca4c74d
Manirul Islam <154756557+devmanirul@users.noreply.github.com>
2024-07-07 00:01:26 +0600

    upaldf

--- script.js
+++ script.js
@@ -10,11 +10,34 @@ $(document).ready(function () {
   });
 });
 
+// flash responsive
+$(document).ready(function () {
+  $(".responsive_cart").slick({
+    // setting-name: setting-value
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: ".prev",
+    nextArrow: ".back",
+  });
+});
+
 // Browse by catagory
+$(document).ready(function () {
+  $(".bbcatagoryCard").slick({
+    // setting-name: setting-value
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: ".previous",
+    nextArrow: ".next",
+  });
+});
+// responsive Browse by catagory
 $(document).ready(function () {
   $(".catagoryCard").slick({
     // setting-name: setting-value
-    slidesToShow: 6,
+    slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
     prevArrow: ".Cprevious",
